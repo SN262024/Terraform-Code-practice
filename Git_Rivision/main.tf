28b1b856198e2de7fe24c24a5e3c6992178fe50f
SN262024 <surendrachauhan18@gmail.com>
2025-08-20 17:13:25 +0530

    added resource group & storageaccount

---
+++ Git_Rivision/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+}
+
+resource "azurerm_resource_group" "rg_us" {
+  name     = "rg-us"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "stg" {
+  name                     = "storageus"
+  resource_group_name      = azurerm_resource_group.rg_us.name
+  location                 = azurerm_resource_group.rg_us.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
