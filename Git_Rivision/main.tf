8f2e6258abc97be0746764ea8b906d5a72d52c01
SN262024 <surendrachauhan18@gmail.com>
2025-08-21 16:42:50 +0530

    Added resource group and storage account

--- Git_Rivision/main.tf
+++ Git_Rivision/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_resource_group" "rg_Nik" {
   name     = "rg-Nik"
   location = "West Europe"
 }
-resource "azurerm_storage_account" "stg1" {
-  name                     = "storageus"
+resource "azurerm_storage_account" "stg12" {
+  name                     = "storageaccountnik"
   resource_group_name      = azurerm_resource_group.rg_Nik.name
   location                 = azurerm_resource_group.rg_Nik .location
   account_tier             = "Standard"
