6bc7e35ed996c838b0134d029966fab123abac3d
SN262024 <surendrachauhan18@gmail.com>
2025-08-22 15:02:19 +0530

    Added RG ,Stg & Backend block

--- Git_Rivision/main.tf
+++ Git_Rivision/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.40.0"
     }
   }
+  backend "azurerm" {
+   resource_resource_group_name ="rg-nikasi1" 
+    storage_account_name = "stgnikasi1"                                 
+    container_name       = "containernikasi"                                  
+    key                  = "dev.tfstate"                   
+  }
 }
 
 provider "azurerm" {
@@ -12,14 +18,14 @@ provider "azurerm" {
   subscription_id ="177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
 }
 
-resource "azurerm_resource_group" "rg_Nikasi" {
-  name     = "rg-Nikasi"
+resource "azurerm_resource_group" "rg_Nikasi2" {
+  name     = "rg-Nikasi2"
   location = "West Europe"
 }
-resource "azurerm_storage_account" "stg123" {
-  name                     = "storageaccountnikasi"
-  resource_group_name      = azurerm_resource_group.rg_Nikasi.name
-  location                 = azurerm_resource_group.rg_Nikasi.location
+resource "azurerm_storage_account" "stg1234" {
+  name                     = "storageaccountnikasi1"
+  resource_group_name      = azurerm_resource_group.rg_Nikasi2.name
+  location                 = azurerm_resource_group.rg_Nikasi2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
