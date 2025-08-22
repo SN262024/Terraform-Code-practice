24b061299a890fcc3f75ef055500b954a53b0f97
SN262024 <surendrachauhan18@gmail.com>
2025-08-22 16:23:33 +0530

    Backend block change

--- Git_Rivision/main.tf
+++ Git_Rivision/main.tf
@@ -6,26 +6,26 @@ terraform {
     }
   }
   backend "azurerm" {
-   resource_group_name ="rg-nikasi2" 
-    storage_account_name = "stgnikasi1"                                 
-    container_name       = "containernikasi"                                  
-    key                  = "dev.tfstate"                   
+    resource_group_name  = "rg-nikasi1"
+    storage_account_name = "stgnikasi1"
+    container_name       = "containernikasi"
+    key                  = "dev.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id ="177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+  subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
 }
 
-resource "azurerm_resource_group" "rg_Nikasi2" {
-  name     = "rg-Nikasi2"
+resource "azurerm_resource_group" "rg_Nikasi22" {
+  name     = "rg-Nikasi22"
   location = "West Europe"
 }
-resource "azurerm_storage_account" "stg1234" {
-  name                     = "storageaccountnikasi1"
-  resource_group_name      = azurerm_resource_group.rg_Nikasi2.name
-  location                 = azurerm_resource_group.rg_Nikasi2.location
+resource "azurerm_storage_account" "stg1111" {
+  name                     = "storageaccountnikasi22"
+  resource_group_name      = azurerm_resource_group.rg_Nikasi22.name
+  location                 = azurerm_resource_group.rg_Nikasi22.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
